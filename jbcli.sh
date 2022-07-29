f3b7a521636453d7a16caefe212676ab3db07876
Jeremy Britt <jeremy.britt@oddball.io>
2022-07-29 12:05:51 -0400

    add initial rds info

--- jbcli.sh
+++ jbcli.sh
@@ -6,6 +6,20 @@ set -e
 
 TYPE=$(gum choose "info" "tag" "create" "delete" "IAM password")
 
+if [ "$TYPE" == "info" ]; then
+  AWSSERVICE=$(gum choose "ec2" "rds")
+fi
+
+if [ "$AWSSERVICE" == "rds" ]; then
+  VAGOVSERVICE=$(gum choose "vets-api" "sentry" "jenkins")
+  if [ "$VAGOVSERVICE" == "vets-api" ]; then
+    RDSINFO=$(gum choose "identifiers" "engine versions")
+    if [ "$RDSINFO" == "engine versions" ]; then
+      for db in $(aws rds describe-db-instances | jq -r '.DBInstances[].DBInstanceIdentifier' | grep $VAGOVSERVICE); do echo $db && aws rds describe-db-instances --db-instance-identifier $db | jq -r '.DBInstances[] | .Engine, .EngineVersion' && echo ""; done
+    fi
+  fi
+fi
+
 if [ "$TYPE" == "IAM password" ]; then
   USERPASS=$(gum choose "create first-time login credentials" "update existing password")
 fi
