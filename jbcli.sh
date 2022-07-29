e83619dc413306d4fc1229e4d9923ae6dd27484a
Jeremy Britt <jeremy.britt@oddball.io>
2022-07-29 12:56:09 -0400

    tweaks

--- jbcli.sh
+++ jbcli.sh
@@ -11,13 +11,15 @@ if [ "$TYPE" == "info" ]; then
 fi
 
 if [ "$AWSSERVICE" == "rds" ]; then
-  VAGOVSERVICE=$(gum choose "vets-api" "sentry" "jenkins")
-  if [ "$VAGOVSERVICE" == "vets-api" ]; then
-    RDSINFO=$(gum choose "engine versions")
-    if [ "$RDSINFO" == "engine versions" ]; then
-      gum spin -s dot --title "Working on it..." -- sleep 3 && \
-      for db in $(aws rds describe-db-instances | jq -r '.DBInstances[].DBInstanceIdentifier' | grep $VAGOVSERVICE); do echo $db && aws rds describe-db-instances --db-instance-identifier $db | jq -r '.DBInstances[] | .Engine, .EngineVersion' && echo ""; done
-    fi
+  RDSINFO=$(gum choose "engine versions")
+  if [ "$RDSINFO" == "engine versions" ]; then
+    VAGOVSERVICE=$(gum choose "vets-api" "sentry" "cms" "gi-bill-data-service" "grafana" "keycloak") && \
+    gum spin -s dot --title "Working on it..." -- sleep 3 && \
+    for db in $(aws rds describe-db-instances | \
+    jq -r '.DBInstances[].DBInstanceIdentifier' | \
+    grep $VAGOVSERVICE | grep -wv appeals); do echo $db && \
+    aws rds describe-db-instances --db-instance-identifier $db | \
+    jq -r '.DBInstances[] | .Engine, .EngineVersion' && echo ""; done
   fi
 fi
 
@@ -27,18 +29,18 @@ fi
 
 if [ "$USERPASS" == "create first-time login credentials" ]; then
   IAMUSER=$(gum input --placeholder "IAM username") && \
-	  gum confirm "Create temporary credentials for $IAMUSER?" && \
-	  gum spin -s dot --title "Working on it..." -- sleep 3 && \
-	  export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!" && \
-	  echo "Temporary password is: $TEMPPASS" && \
-	  aws iam create-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
+	gum confirm "Create temporary credentials for $IAMUSER?" && \
+	gum spin -s dot --title "Working on it..." -- sleep 3 && \
+	export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!" && \
+	echo "Temporary password is: $TEMPPASS" && \
+	aws iam create-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
 fi
 
 if [ "$USERPASS" == "update existing password" ]; then
   IAMUSER=$(gum input --placeholder "IAM username") && \
-          gum confirm "Update existing password for $IAMUSER?" && \
-          gum spin -s dot --title "Working on it..." -- sleep 3 && \
-          export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!" && \
-          echo "Temporary password is: $TEMPPASS" && \
-          aws iam update-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
+  gum confirm "Update existing password for $IAMUSER?" && \
+  gum spin -s dot --title "Working on it..." -- sleep 3 && \
+  export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!" && \
+  echo "Temporary password is: $TEMPPASS" && \
+  aws iam update-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
 fi
