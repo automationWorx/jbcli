2f7adf3175f51272c54e78b2969443cd9db415af
Jeremy Britt <jeremy.britt@oddball.io>
2022-07-29 11:45:49 -0400

    tweaks

--- jbcli.sh
+++ jbcli.sh
@@ -4,13 +4,26 @@
 
 set -e
 
-TYPE=$(gum choose "info" "tag" "create" "delete" "create temporary IAM user password")
+TYPE=$(gum choose "info" "tag" "create" "delete" "IAM password")
 
-if [ "$TYPE" == "create temporary IAM user password" ]; then
+if [ "$TYPE" == "IAM password" ]; then
+  USERPASS=$(gum choose "create first-time login credentials" "update existing password")
+fi
+
+if [ "$USERPASS" == "create first-time login credentials" ]; then
   IAMUSER=$(gum input --placeholder "IAM username") && \
 	  gum confirm "Create temporary credentials for $IAMUSER?" && \
 	  gum spin -s dot --title "Working on it..." -- sleep 3 && \
 	  export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)" && \
 	  echo "Temporary password is: $TEMPPASS" && \
 	  aws iam create-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
+fi
+
+if [ "$USERPASS" == "update existing password" ]; then
+  IAMUSER=$(gum input --placeholder "IAM username") && \
+          gum confirm "Update existing password for $IAMUSER?" && \
+          gum spin -s dot --title "Working on it..." -- sleep 3 && \
+          export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)" && \
+          echo "Temporary password is: $TEMPPASS" && \
+          aws iam update-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
 fi
