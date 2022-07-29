ccfa96a96540993b5487d469fa4bbf05cfc0b6f9
Jeremy Britt <jeremy.britt@oddball.io>
2022-07-29 10:55:46 -0400

    update

--- jbcli.sh
+++ jbcli.sh
@@ -1,1 +1,10 @@
-<PLACEHOLDER>
+#!/bin/bash
+
+# This script is used to perform numerous awscli tasks a bit easier
+
+set -e
+
+TYPE=$(gum choose "info" "tag" "create" "delete" "create temporary IAM user password")
+IAMUSER=$(gum input --placeholder "IAM username")
+
+gum confirm "Create temporary credentials for $IAMUSER?" && export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)" && echo "Temporary password is: $TEMPPASS"  && aws iam create-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
