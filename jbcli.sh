e741c8044b2e0f22580547d13156d46b96bca6e1
Jeremy Britt <jeremy.britt@oddball.io>
2022-08-05 11:16:01 -0400

    add param store options

--- jbcli.sh
+++ jbcli.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-TYPE=$(gum choose "info" "tag" "create" "delete" "IAM password")
+TYPE=$(gum choose "info" "tag" "create" "delete" "IAM password" "Parameter Store")
 
 ###############################
 ######## Info options #########
@@ -18,11 +18,11 @@ if [ "$AWSSERVICE" == "rds" ]; then
   RDSINFO=$(gum choose "engine versions")
   if [ "$RDSINFO" == "engine versions" ]; then
     VAGOVSERVICE=$(gum choose "vets-api" "sentry" "cms" "gi-bill-data-service" \
-    "console-api" "console-ui" "grafana" "keycloak") && \
-    gum spin -s dot --title "Working on it..." -- sleep 3 && \
+    "console-api" "console-ui" "grafana" "keycloak")
+    gum spin -s dot --title "Working on it..." -- sleep 3
     for db in $(aws rds describe-db-instances | \
     jq -r '.DBInstances[].DBInstanceIdentifier' | \
-    grep $VAGOVSERVICE | grep -wv appeals); do echo $db && \
+    grep $VAGOVSERVICE | grep -wv appeals); do echo $db
     aws rds describe-db-instances --db-instance-identifier $db | \
     jq -r '.DBInstances[] | .Engine, .EngineVersion' && echo ""; done
   fi
@@ -49,19 +49,79 @@ if [ "$TYPE" == "IAM password" ]; then
 fi
 
 if [ "$USERPASS" == "create first-time login credentials" ]; then
-  IAMUSER=$(gum input --placeholder "IAM username") && \
-	gum confirm "Create temporary credentials for $IAMUSER?" && \
-	gum spin -s dot --title "Working on it..." -- sleep 3 && \
-	export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!" && \
-	echo "Temporary password is: $TEMPPASS" && \
+  IAMUSER=$(gum input --placeholder "IAM username")
+	gum confirm "Create temporary credentials for $IAMUSER?"
+	gum spin -s dot --title "Working on it..." -- sleep 3
+	export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!"
+	echo "Temporary password is: $TEMPPASS"
 	aws iam create-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
 fi
 
 if [ "$USERPASS" == "update existing password" ]; then
-  IAMUSER=$(gum input --placeholder "IAM username") && \
-  gum confirm "Update existing password for $IAMUSER?" && \
-  gum spin -s dot --title "Working on it..." -- sleep 3 && \
-  export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!" && \
-  echo "Temporary password is: $TEMPPASS" && \
+  IAMUSER=$(gum input --placeholder "IAM username")
+  gum confirm "Update existing password for $IAMUSER?"
+  gum spin -s dot --title "Working on it..." -- sleep 3
+  export "TEMPPASS=$(curl -sL pwgen.btmn.dev/20)!"
+  echo "Temporary password is: $TEMPPASS"
   aws iam update-login-profile --user-name "$IAMUSER" --password "$TEMPPASS" --password-reset-required
+fi
+
+################################
+##### Param Store options ######
+################################
+
+if [ "$TYPE" == "Parameter Store" ]; then
+  ACTION=$(gum choose "create" "update" "retrieve" "delete")
+fi
+
+if [ "$ACTION" == "create" ]; then
+  VASERVICE=$(gum choose "vets-api" "vets-website")
+    if [ "$VASERVICE" == "vets-api" ]; then
+      ENV=$(gum choose "dev" "staging" "sandbox" "prod")
+      INTSERVICE=$(gum input --placeholder "Underlying Integration Service")
+      if [ "$ENV" == "dev" ]; then
+        KEY=$(gum input --placeholder "Enter key / name of secret")
+        SECRETVALUE=$(gum input --placeholder "Enter secret value")
+        gum confirm "Create new secret with path /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY?"
+        gum spin -s dot --title "Working on it..." -- sleep 3
+        aws ssm put-parameter --name /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY \
+        --type SecureString \
+        --value $SECRETVALUE
+      fi
+      if [ "$ENV" == "staging" ]; then
+        KEY=$(gum input --placeholder "Enter key / name of secret")
+        SECRETVALUE=$(gum input --placeholder "Enter secret value")
+        gum confirm "Create new secret with path /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY"
+        gum spin -s dot --title "Working on it..." -- sleep 3
+        aws ssm put-parameter --name /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY \
+        --type SecureString \
+        --value $SECRETVALUE
+      fi
+      if [ "$ENV" == "sandbox" ]; then
+        KEY=$(gum input --placeholder "Enter key / name of secret")
+        SECRETVALUE=$(gum input --placeholder "Enter secret value")
+        gum confirm "Create new secret with path /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY"
+        gum spin -s dot --title "Working on it..." -- sleep 3
+        aws ssm put-parameter --name /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY \
+        --type SecureString \
+        --value $SECRETVALUE
+      fi
+      if [ "$ENV" == "prod" ]; then
+        KEY=$(gum input --placeholder "Enter key / name of secret")
+        SECRETVALUE=$(gum input --placeholder "Enter secret value")
+        gum confirm "Create new secret with path /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY"
+        gum spin -s dot --title "Working on it..." -- sleep 3
+        aws ssm put-parameter --name /dsva-vagov/$VASERVICE/$ENV/$INTSERVICE/$KEY \
+        --type SecureString \
+        --value $SECRETVALUE
+      fi
+    fi
+fi
+        
+if [ "$ACTION" == "delete" ]; then
+  DELETEPARAM=$(gum input --placeholder "Enter Parameter Store path to value you wish to delete")
+  gum confirm "Delete secret with path $DELETEPARAM?"
+  gum spin -s dot --title "Working on it..." -- sleep 3
+  aws ssm delete-parameter --name $DELETEPARAM
+  echo "Parameter deleted!"
 fi
